656f5d2ccbd17543acdade09742aa23d0482808c
aronmakiika <aronmakiika@gmail.com>
2022-02-19 08:38:15 +0300

    refactor: Add to class-list folder

---
+++ src/class-list/index.js
@@ -0,0 +1,18 @@
+
+const h1 = document.getElementById("h1");
+const h2 = document.getElementById("h2");
+const h3 = document.getElementById("h3");
+
+Blue = ()=> {
+ h1.classList.add("blue-text")
+ console.log("button clicked");
+}
+
+Red = ()=> {
+    h2.classList.add("red-text")
+    
+}
+
+Green = ()=> {
+    h3.classList.add("green-text")
+}
